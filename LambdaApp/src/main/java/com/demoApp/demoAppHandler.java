80f41bc5b00576dba0e43c34386c35853208f6ff
Kamat <bkkamat@8c85904b5072.ant.amazon.com>
2018-05-07 21:40:38 -0400

    Lambda test app

---
+++ LambdaApp/src/main/java/com/demoApp/demoAppHandler.java
@@ -0,0 +1,17 @@
+package com.demoApp;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class demoAppHandler implements RequestHandler<Object, String> {
+
+	@Override
+    public String handleRequest(Object input, Context context) {
+        context.getLogger().log("Input: " + input);
+
+        // TODO: implement your handler
+        return "Hello from Lambda";
+    }
+    
+}
+
---
+++ LambdaApp/src/test/java/com/demoApp/testdemoApp.java
@@ -0,0 +1,21 @@
+package com.demoApp;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testdemoApp {
+
+	@Before
+    
+    @After
+    
+    @Test
+    public void testGetAllOrganizations() {
+        
+        assertThat("Hello", is("Hello"));
+    }
+}
